2480b0dd7873b20377b92ae4c780d3629c5bbb65
Luk <luk@airframe.io>
2020-09-11 13:20:39 +0200

    add random() request fn + add mocha tests

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-module.exports = () => {
-  console.log('cat facts test')
+const fetch = require("node-fetch")
+
+const API_URL = 'https://cat-fact.herokuapp.com/facts'
+
+const random = (amount = 1) => {
+  amount = amount > 10 ? 10 : amount
+  return get(`${API_URL}/random?amount=${amount}`)
 }
+
+const get = async resource => {
+  const response = await fetch(resource)
+
+  if (response.ok) {
+    const data = await response.json()
+    return data
+  } else {
+    return response.status
+  }
+}
+
+exports.random = random
---
+++ index.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert')
+const catFacts = require('./index')
+
+describe('random()', function () {
+  it('returns 1 cat fact', async function () {
+    const data = await catFacts.random()
+    assert.equal(typeof data, 'object')
+  })
+
+  it('returns 3 cat facts', async function () {
+    const data = await catFacts.random(3)
+    assert.equal(data.length, 3)
+  })
+})
